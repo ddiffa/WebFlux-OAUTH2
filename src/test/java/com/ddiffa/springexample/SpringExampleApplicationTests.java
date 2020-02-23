1c3857c398cf03e2155fd6b0b980b43fcdde4b3a
Diffa <ddiffa2@gmail.com>
2020-02-23 23:01:59 +0700

    updated

---
+++ src/test/java/com/ddiffa/springexample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.ddiffa.springexample;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringExampleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
