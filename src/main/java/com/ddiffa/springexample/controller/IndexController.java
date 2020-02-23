d68738c668a50820cc2e304a1a95a2c835b98228
Diffa <ddiffa2@gmail.com>
2020-02-23 23:15:24 +0700

    create index

---
+++ src/main/java/com/ddiffa/springexample/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.ddiffa.springexample.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index(){
+        return "Oatuh2 Spring Boot";
+    }
+}
